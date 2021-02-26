cd85a76abf764535552f09787271a839ccf02fa2
cairo387 <mifuneri@outlook.jp>
2021-02-26 11:22:38 +0900

    [Add]HTMLのCSS要素を操作

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>')
+/*$(function(){
+  $('.box1').slideDown();
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px',
+    'width': '200px'
+  });
+  $('.box1').slideUp();
+});*/
+
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
 
 //$( セレクタ ).メソッド ( パラメータ );
 //セレクタ：body
 //メソッド：html
 //パラメータ'<h1>Hello jQuery!!</h1>'
+
+//slideDown(速度);: 上から下へ(現れる).単位はミリ秒.1秒の場合は'1000'
+//slideUp(速度);: 下から上へ(消えていく)
+//show();:非表示状態にあるものを表示
+//hide();:一瞬だけ表示
